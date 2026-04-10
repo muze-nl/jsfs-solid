141688af7a6497822a8b50e2e0a697b552d91656
Auke van Slooten <auke@muze.nl>
2026-04-10 15:00:06 +0200

    updated to return both client and adapter

--- src/browser.js
+++ src/browser.js
@@ -1,5 +1,16 @@
+import '@muze-nl/jsfs'
+import '@muze-nl/metro'
+import '@muze-nl/metro-oidc'
+import '@muze-nl/oldm'
+import '@muze-nl/jaqt'
+import '@muze-nl/assert'
 import SolidAdapter from './SolidAdapter.js'
+import solidClient from './SolidClient.js'
 
-export default SolidAdapter
+export default {
+	adapter: SolidAdapter,
+	client: solidClient
+}
 
+globalThis.solidClient = solidClient
 globalThis.SolidAdapter = SolidAdapter
