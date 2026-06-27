0c8183f1023cf7687a86f34fea83b4927e74a781
Auke van Slooten <auke@muze.nl>
2026-06-27 14:49:55 +0200

    prefixes are now set by oldm by default

--- src/SolidClient.js
+++ src/SolidClient.js
@@ -6,30 +6,7 @@ import '@muze-nl/jaqt'
 import '@muze-nl/assert'
 import SolidAdapter from './SolidAdapter.js'
 
-export default async function solidClient(webid, solidOptions) {
-    const defaults = {
-		prefixes: {
-	        'ldp':    'http://www.w3.org/ns/ldp#',
-	        'rdf':    'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-	        'dct':    'http://purl.org/dc/terms/',
-	        'stat':   'http://www.w3.org/ns/posix/stat#',
-	        'turtle': 'http://www.w3.org/ns/iana/media-types/text/turtle#',
-	        'schema': 'https://schema.org/',
-	        'solid':  'http://www.w3.org/ns/solid/terms#',
-	        'acl':    'http://www.w3.org/ns/auth/acl#',
-	        'space':  'http://www.w3.org/ns/pim/space#',
-	        'vcard':  'http://www.w3.org/2006/vcard/ns#',
-	        'foaf':   'http://xmlns.com/foaf/0.1/'
-	    },
-	    parser: oldm.n3Parser,
-	    writer: oldm.n3Writer
-    }
-    const options = Object.assign({}, defaults, solidOptions)
-    for (const prefix in defaults.prefixes) {
-    	if (!options.prefixes[prefix]) {
-    		options.prefixes[prefix] = defaults.prefixes[prefix]
-    	}
-    }
+export default async function solidClient(webid, options) {
 	const response = await metro.client(oldmmw(options), metro.mw.getdata()).get(webid)
 	const profile = response?.primary
 	if (!profile || !profile.solid$oidcIssuer) { //FIXME: don't assume $ as the separator
